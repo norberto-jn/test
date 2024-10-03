3822be59c118f09b1556fbe147b88c30b9022a23
Norberto Ferreira <norbertojunio@gmail.com>
2024-10-03 17:25:40 -0300

    add

---
+++ test.ts
@@ -0,0 +1,11 @@
+class Calculadora {
+    // Função para somar dois números
+    somar(a: number, b: number): number {
+        return a + b;
+    }
+
+    // Função para subtrair dois números
+    subtrair(a: number, b: number): number {
+        return a - b;
+    }
+}
